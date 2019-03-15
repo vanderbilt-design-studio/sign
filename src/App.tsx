da4d4ac0878d8b29828ee56ed1fa991e1064d4f5
Sameer Puri <sameer@users.noreply.github.com>
2019-03-15 01:02:07 -0500

    Use websocket, reduce h3 font vw

--- src/App.tsx
+++ src/App.tsx
@@ -26,17 +26,16 @@ interface ISignState {
   updateTimeIntervalId?: NodeJS.Timeout;
   time?: string;
 
-  lastUpdate?: IMessage;
+  messageSocket: WebSocket;
+  lastMessage?: IMessage;
 }
 
 class App extends Component<any, ISignState> {
   constructor(props: any) {
     super(props);
-    this.state = {lastUpdate: {
-      open: true,
-      mentors: ["Daiwei L", "Sameer P", "Christina H"],
-      weather: "⛅️ 🌡️+73°F 🌬️↑23 mph"
-    }};
+    const messageSocket = new WebSocket("wss://iot.vanderbilt.design/sign");
+    messageSocket.onmessage = msg => this.setState({lastMessage: JSON.parse(msg.data) as IMessage});
+    this.state = {messageSocket};
     this.updateTime = this.updateTime.bind(this);
   }
 
@@ -55,6 +54,8 @@ class App extends Component<any, ISignState> {
     if (this.state.updateTimeIntervalId) {
       clearInterval(this.state.updateTimeIntervalId);
     }
+
+    this.state.messageSocket.close();
   }
 
   public render() {
@@ -62,33 +63,41 @@ class App extends Component<any, ISignState> {
       <div
         className="app"
         style={{
-          backgroundColor: this.state.lastUpdate
-            ? this.state.lastUpdate.open
+          backgroundColor: this.state.lastMessage
+            ? this.state.lastMessage.open
               ? Colors.GREEN
               : Colors.RED
             : Colors.WHITE,
-          color: this.state.lastUpdate ? Colors.WHITE : Colors.BLACK
+          color: this.state.lastMessage ? Colors.WHITE : Colors.BLACK
         }}
       >
         <h2>Design Studio</h2>
         <h1>
-          {this.state.lastUpdate
-            ? this.state.lastUpdate.open
+          {this.state.lastMessage
+            ? this.state.lastMessage.open
               ? "Open"
               : "Closed"
             : "Unknown"}
         </h1>
         <div className="row">
-          {this.state.lastUpdate && this.state.lastUpdate.mentors && this.state.lastUpdate.mentors.length !== 0 && (
-            <h3 className="mentors">
-              Mentor{this.state.lastUpdate.mentors.length > 1 ? "s" : ""} on
-              Duty:
-              <ul>
-              {this.state.lastUpdate.mentors.map(mentor => <li>{mentor}</li>)}
-              </ul>
-            </h3>
-          )}
-          <h3 className="time">{this.state.time}<br/>{this.state.lastUpdate && this.state.lastUpdate.weather}</h3>
+          {this.state.lastMessage &&
+            this.state.lastMessage.mentors &&
+            this.state.lastMessage.mentors.length !== 0 && (
+              <h3 className="mentors">
+                Mentor{this.state.lastMessage.mentors.length > 1 ? "s" : ""} on
+                Duty:
+                <ul>
+                  {this.state.lastMessage.mentors.map(mentor => (
+                    <li>{mentor}</li>
+                  ))}
+                </ul>
+              </h3>
+            )}
+          <h3 className="time">
+            {this.state.time}
+            <br />
+            {this.state.lastMessage && this.state.lastMessage.weather}
+          </h3>
         </div>
       </div>
     );
